ed615738d0608e7ad7ce1bfbca83658d93e8a64d
Harsh <95859326+harsh5ingh@users.noreply.github.com>
2025-03-09 01:14:46 +0530

    index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const App = (props) => {
   
   // CHALLENGE:
   // Change the query to one of your interests
-  let [query, setQuery] = useState("");
+  let [query, setQuery] = useState("Marvel");
   const queryInput = useRef(null);
 
   const numberOfPhotos = 20;
@@ -79,4 +79,4 @@ const App = (props) => {
 };
 
 // CHALLENGE: add your own name and emoji to the website
-ReactDOM.render(<App name="Per" emoji="🧛‍♂️"/>, document.getElementById("root"));
+ReactDOM.render(<App name="Harsh" emoji="🦹🏻"/>, document.getElementById("root"));
